e7329e90300b8ce175ebd5d961760dac12b97c60
Patrick Hackman <pajohack@gmail.com>
2023-09-14 18:39:49 +0000

    Add sorting capability for price & category

--- products/views.py
+++ products/views.py
@@ -11,8 +11,24 @@ def all_products(request):
     books = Book.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'title':  # Changed from 'name' to 'title'
+                sortkey = 'lower_title'
+                books = books.annotate(lower_title=Lower('title'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            books = books.order_by(sortkey)  # Now updating the 'books' queryset
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             books = books.filter(category__name__in=categories)
@@ -23,14 +39,17 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(title__icontains=query) | Q(author__name__icontains=query)
             books = books.filter(queries)
+
+    current_sorting = f'{sort}_{direction}' if sort and direction else 'default'
 
     context = {
         'books': books,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/books.html', context)
