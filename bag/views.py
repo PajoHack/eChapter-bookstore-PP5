946358a2cafdeb76fcc15dae039b0e409aea1a20
Patrick Hackman <pajohack@gmail.com>
2023-09-17 19:17:55 +0000

    Add to bag functionality created

--- bag/views.py
+++ bag/views.py
@@ -12,7 +12,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified book to the shopping bag """
-    book = get_object_or_404(Book, pk=item_id)  # Changed from Product to Book
+    book = get_object_or_404(Book, pk=item_id) 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -20,31 +20,32 @@ def add_to_bag(request, item_id):
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
         messages.success(request,
-                         (f'Updated {book.title} '  # Changed from product.name to book.title
+                         (f'Updated {book.title} '
                           f'quantity to {bag[item_id]}'))
     else:
         bag[item_id] = quantity
-        messages.success(request, f'Added {book.title} to your bag')  # Changed from product.name to book.title
+        messages.success(request, f'Added {book.title} to your bag')
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified book to the specified amount"""
-    book = get_object_or_404(Book, pk=item_id)  # Changed from Product to Book
+    book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
         messages.success(request,
-                         (f'Updated {book.title} '  # Changed from product.name to book.title
+                         (f'Updated {book.title} '
                           f'quantity to {bag[item_id]}'))
     else:
         bag.pop(item_id)
         messages.success(request,
-                         (f'Removed {book.title} '  # Changed from product.name to book.title
+                         (f'Removed {book.title} '
                           f'from your bag'))
 
     request.session['bag'] = bag
@@ -54,11 +55,11 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
     try:
-        book = get_object_or_404(Book, pk=item_id)  # Changed from Product to Book
+        book = get_object_or_404(Book, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-        messages.success(request, f'Removed {book.title} from your bag')  # Changed from product.name to book.title
+        messages.success(request, f'Removed {book.title} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
